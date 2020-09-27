ee80e4504a5d03a77ac6545c6f3d20b69c1e456f
wish2023 <vishesh@u.nus.edu>
2020-09-27 19:42:43 +0800

    Remove magic numbers from public methods and variables

--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -2,6 +2,59 @@ package duke;
 
 public class Parser {
 
+    private static final String SPACE = " ";
+    private static final int START = 0;
+    private static final String TASK_START_KEYWORD = ". [";
+    private static final String BY_KEYWORD_FILE = "(by";
+    private static final String AT_KEYWORD_FILE = "(at";
+    public static final String BY_KEYWORD_INPUT = "/by";
+    public static final String AT_KEYWORD_INPUT = "/at";
+
+    private static int getDividerPosition(String line) throws DukeException {
+        int dividerPosition = getIndexOfKeyword(line, SPACE);
+        return dividerPosition;
+    }
+
+    private static int getIndexOfKeyword(String line, String keyword) throws DukeException {
+        if (!line.contains(keyword)) {
+            throw new DukeException();
+        }
+        return line.indexOf(keyword);
+    }
+
+    private static String getDateFromInput(String line, int dividerPosition) {
+        return line.substring(dividerPosition + 4);
+    }
+
+    private static String getTaskFromInput(String line, int startPosition, int endPosition) {
+        return line.substring(startPosition + 1, endPosition - 1);
+    }
+
+    private static String getTodoFromFile(String line, int taskStartPosition) {
+        return line.substring(taskStartPosition + 9);
+    }
+
+    private static String getTaskFromFile(String line, int startPosition, int endPosition) {
+        return line.substring(startPosition + 9, endPosition - 1);
+    }
+
+    private static char getStatusIconFromFile(String line, int taskStartPosition) {
+        return line.charAt(taskStartPosition + 6);
+    }
+
+    private static char getTaskTypeFromFile(String line, int taskStartPosition) {
+        return line.charAt(taskStartPosition + 3);
+    }
+
+    private static String getDateFromFile(String line, int datePosition) {
+        return line.substring(datePosition + 5, line.length() - 1);
+    }
+
+    private static boolean isInstanceEmpty(String instance) {
+        return instance.length() == 0;
+    }
+
+
 
     /**
      * Returns a task given a line from duke.txt
@@ -11,42 +64,34 @@ public class Parser {
      * @throws DukeException If file is in incorrect format
      */
     public static Task getTaskFromLine(String line) throws DukeException { // For file
-        int taskTypePosition = getIndexOfKeyword(line, ". [");
-        char taskType = line.charAt(taskTypePosition + 3);
-        char statusIcon = line.charAt(taskTypePosition + 6);
+        int taskStartPosition = getIndexOfKeyword(line, TASK_START_KEYWORD);
+        char taskType = getTaskTypeFromFile(line, taskStartPosition);
+        char statusIcon = getStatusIconFromFile(line, taskStartPosition);
 
         Task task = null;
 
         switch (taskType) {
         case 'T':
-            String todoTask = line.substring(taskTypePosition + 9);
+            String todoTask = getTodoFromFile(line, taskStartPosition);
             task = new Todo(todoTask, statusIcon);
             break;
         case 'D':
-            int byPosition = getIndexOfKeyword(line, "(by");
-            String deadlineTask = line.substring(taskTypePosition + 9, byPosition - 1);
-            String by = line.substring(byPosition + 5, line.length() - 1);
-            task = new Deadline(deadlineTask, statusIcon, by);
+            int byPosition = getIndexOfKeyword(line, BY_KEYWORD_FILE);
+            String deadlineTask = getTaskFromFile(line, taskStartPosition, byPosition);
+            String deadlineDate = getDateFromFile(line, byPosition);
+            task = new Deadline(deadlineTask, statusIcon, deadlineDate);
             break;
         case 'E':
-            int atPosition = getIndexOfKeyword(line, "(at");
-            String eventTask = line.substring(taskTypePosition + 9, atPosition - 1);
-            String at = line.substring(atPosition + 5, line.length() - 1);
-            task = new Event(eventTask, statusIcon, at);
+            int atPosition = getIndexOfKeyword(line, AT_KEYWORD_FILE);
+            String eventTask = getTaskFromFile(line, taskStartPosition, atPosition);
+            String eventDate = getDateFromFile(line, atPosition);
+            task = new Event(eventTask, statusIcon, eventDate);
             break;
         default:
             break;
         }
 
         return task;
-    }
-
-
-    private static int getIndexOfKeyword(String line, String keyword) throws DukeException {
-        if (!line.contains(keyword)) {
-            throw new DukeException();
-        }
-        return line.indexOf(keyword);
     }
 
 
@@ -61,18 +106,12 @@ public class Parser {
         String command;
         try {
             dividerPosition = getDividerPosition(line);
-            command = line.substring(0, dividerPosition);
+            command = line.substring(START, dividerPosition);
         } catch (DukeException e) {
-            command = line; // Line is only 1 word
+            command = line; // Command is only 1 word
         }
 
         return command;
-    }
-
-    private static int getDividerPosition(String line) throws DukeException {
-        int dividerPosition = 0;
-        dividerPosition = getIndexOfKeyword(line, " ");
-        return dividerPosition;
     }
 
 
@@ -87,12 +126,13 @@ public class Parser {
         int dividerPosition = getDividerPosition(line);
 
         String instance = line.substring(dividerPosition + 1);
-        if (instance.length() == 0) {
+        if (isInstanceEmpty(instance)) {
             throw new DukeException();
         }
 
         return instance;
     }
+
 
     /**
      * Creates a deadline based on user input
@@ -103,11 +143,12 @@ public class Parser {
      */
     public static Deadline getDeadline(String line) throws DukeException {
         int dividerPosition = getDividerPosition(line);
-        int byPosition = getIndexOfKeyword(line, "/by");;
-        String deadlineTask = line.substring(dividerPosition + 1, byPosition - 1);
-        String by = line.substring(byPosition + 4);
-        return new Deadline(deadlineTask, by);
+        int byPosition = getIndexOfKeyword(line, BY_KEYWORD_INPUT);;
+        String deadlineTask = getTaskFromInput(line, dividerPosition, byPosition);
+        String date = getDateFromInput(line, byPosition);
+        return new Deadline(deadlineTask, date);
     }
+
 
     /**
      * Creates an event based on user input
@@ -118,10 +159,12 @@ public class Parser {
      */
     public static Event getEvent(String line) throws DukeException {
         int dividerPosition = getDividerPosition(line);
-        int atPosition = getIndexOfKeyword(line, "/at");
-        String eventTask = line.substring(dividerPosition + 1, atPosition - 1);
-        String date = line.substring(atPosition + 4);
+        int atPosition = getIndexOfKeyword(line, AT_KEYWORD_INPUT);
+        String eventTask = getTaskFromInput(line, dividerPosition, atPosition);
+        String date = getDateFromInput(line, atPosition);
         return new Event(eventTask, date);
     }
+
+
 
 }
